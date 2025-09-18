61ab83d2f4b983fec6aeedddcb1fa7482103b921
micakucing <ramahardiansidik@gmail.com>
2025-09-18 12:06:45 +0700

    l

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthProvider } from '../context/AuthContext';
+import Layout from '../components/Layout';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
+  );
 }
